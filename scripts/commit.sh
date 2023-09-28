3d73e071d0d51cb31db4d690fc90046c8c1579ea
maxbolgarin <maxbolgarin@yandex.ru>
2023-09-28 11:47:31 +0300

    refactor(fsdf): fwef

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -316,7 +316,7 @@ stat=$(git show $commit_hash --stat --format="" | cat)
 IFS=$'\n' read -rd '' -a stats <<<"$stat"
 for index in "${!stats[@]}"
 do
-    s=$(${stats[index]} | xargs)
+    s=$(echo ${stats[index]} | xargs)
     echo -e "${s}" 
 
 done
